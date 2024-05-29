83fc827aa96346e4b8b823f5f3867b3901b35ad3
Aliaksandr Karpenka <kasasha@mail.ru>
2024-05-29 11:44:48 +0300

    add StorageApp

---
+++ HW03/StorageApp/Controllers/StorageController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StorageApp.DB;
+using Storage = StorageApp.Model.Storage;
+
+namespace StorageApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StorageController : ControllerBase
+    {
+        [HttpPost(template: "addstorage")]
+        public ActionResult AddStorage(string name)
+        {
+            try
+            {
+                using (var ctx = new StorageContext())
+                {
+                    if (ctx.Storages.Any(x => x.Name.ToLower() == name.ToLower()))
+                    {
+                        return StatusCode(409);
+                    }
+                    else
+                    {
+                        ctx.Storages.Add(new Storage { Name = name });
+                        ctx.SaveChanges();
+                    }
+                }
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpGet(template: "getstorages")]
+        public ActionResult<IEnumerable<Storage>> GetStorages()
+        {
+            try
+            {
+                using (var ctx = new StorageContext())
+                {
+                    var list = ctx.Storages.Select(x => new Storage { Id = x.Id, Name = x.Name}).ToList();
+                    return list;
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete(template: "deletestorage")]
+        public ActionResult DeleteStorage(string name)
+        {
+            try
+            {
+                using (var ctx = new StorageContext())
+                {
+                    var storage = ctx.Storages.FirstOrDefault(x => x.Name == name);
+                    if (storage != null)
+                    {
+                        ctx.Storages.Remove(storage);
+                        ctx.SaveChanges();
+                        return Ok();
+                    }
+                    return BadRequest();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ HW03/StorageApp/Migrations/20240521132557_StorageInit.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace StorageApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class StorageInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "storages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("storage_pkey", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "storages");
+        }
+    }
+}
